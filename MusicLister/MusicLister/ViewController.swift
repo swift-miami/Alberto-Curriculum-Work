f33017c87c5fa08a2c743316a08090bffdeeae4b
Alberto <Camacho>
2019-07-07 02:16:14 -0400

    Created the tableview with basic functionality

--- MusicLister/MusicLister/ViewController.swift
+++ MusicLister/MusicLister/ViewController.swift
@@ -8,13 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    @IBOutlet var tableview: UITableView!
+    let myArray = ["Alberto", "Armando", "Gio", "Luis"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myArray.count
+    }
 
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "customcell", for: indexPath)
+        cell.textLabel?.text = myArray[indexPath.item]
+        return cell
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        return tableview.reloadData()
+    }
 
 }
 
